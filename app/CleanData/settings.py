cd1710ea4fba9315513a95e72b8dbc5a3eec8ab1
kirankoduru <crazycreature11@gmail.com>
2013-07-19 15:18:31 -0400

    config settings

--- app/CleanData/settings.py
+++ app/CleanData/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '--------',                      # Or path to database file if using sqlite3.
-        'USER': '--------',                      # Not used with sqlite3.
-        'PASSWORD': '--------',                  # Not used with sqlite3.
-        'HOST': '------',                      # Set to empty string for localhost. Not used with sqlite3.
+        'NAME': 'cleandata',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
@@ -50,7 +50,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/jr_hack/cleandata.jrsandbox.com/public/media'
+MEDIA_ROOT = '/home/kiran/workspace/cleandata/app/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -61,7 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''#os.path.join(os.path.dirname(__file__), 'static/')
+STATIC_ROOT = '/home/kiran/workspace/cleandata/app/static'#os.path.join(os.path.dirname(__file__), 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'CleanData.urls'
 WSGI_APPLICATION = 'CleanData.wsgi.application'
 
 TEMPLATE_DIRS = (
-	"/home/jr_hack/cleandata.jrsandbox.com/templates"
+    "/home/kiran/workspace/cleandata/app/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
