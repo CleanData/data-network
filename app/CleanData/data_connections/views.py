2413bfb0458bc718006613e931f248da321364bc
kirankoduru <crazycreature11@gmail.com>
2013-07-18 20:41:54 -0400

    base template

--- app/CleanData/data_connections/views.py
+++ app/CleanData/data_connections/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
 	#p = get_object_or_404(Poll, pk=poll_id)
 	return render_to_response('data_connections/index.html', {})
+
+def add_dataset(request):
+	return render_to_response('data_connections/add_dataset.html',{})
