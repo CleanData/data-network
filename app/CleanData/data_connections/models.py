574395d63656a9199343911003eab4d08e950d76
jonroberts <jproberts00@gmail.com>
2013-07-29 08:40:30 -0700

    code cleanup, model adjustment and base fixtures added.

--- app/CleanData/data_connections/models.py
+++ app/CleanData/data_connections/models.py
@@ -2,35 +2,56 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # The data models
+class DataCatalogManager(models.Manager):
+	def get_by_natural_key(self,name):
+		return self.get(name=name)
 class DataCatalog(models.Model):
-	name = models.CharField(max_length=200)
+	objects = DataCatalogManager()	# handles natural keys
+
+	name = models.CharField(max_length=200, unique=True)
 	manager = models.ForeignKey("UserProfile",related_name="managed_datacatalogs")
 	managing_organization = models.ForeignKey("Organization",related_name="managed_datacatalogs",null=True,blank=True)
 	
 	def __unicode__(self):
 		return self.name
 
+class LicenseManager(models.Manager):
+	def get_by_natural_key(self,name):
+		return self.get(name=name)
 class License(models.Model):
-	name = models.CharField(max_length=200)
+	objects = LicenseManager() 		# handles natural keys
+
+	name = models.CharField(max_length=200,unique=True)
 	url = models.URLField(blank=True)
 	def __unicode__(self):
 		return self.name
 
+class FormatManager(models.Manager):
+	def get_by_natural_key(self,name):
+		return self.get(name=name)
 class Format(models.Model):
-	name = models.CharField(max_length=200)
+	objects = FormatManager() 		# handles natural keys
+
+	name = models.CharField(max_length=200, unique=True)
 	url = models.URLField(blank=True)
 	def __unicode__(self):
 		return self.name
-	
+
+# sorts out natural identification of datasets
+class DatasetManager(models.Manager):
+	def get_by_natural_key(self,url,name):
+		return self.get(url=url,name=name)
 class Dataset(models.Model):
+	objects = DatasetManager()		# handles natural keys
+	
 	data_catalog = models.ForeignKey(DataCatalog,null=True,blank=True)
-	name = models.CharField(max_length=200)
+	name = models.CharField(max_length=100)
 	date_published = models.DateTimeField('date published')
 	date_last_edited = models.DateTimeField('date last edited')
-	url = models.URLField()
+	url = models.URLField(max_length=150)
 	description = models.TextField(blank=True)
 
-	uploaded_by = models.CharField(max_length=200,blank=True)
+	#added_by = models.CharField(max_length=200,blank=True,)
 
 	# relationships
 	data_format = models.ForeignKey(Format,related_name="formatted_datasets",null=True,blank=True)
@@ -39,33 +60,58 @@ class Dataset(models.Model):
 									related_name='derivatives',null=True,blank=True)
 	contributors = models.ManyToManyField('UserProfile', through='ContributorRelation', symmetrical=False,
 									related_name='contributed_datasets',null=True,blank=True)
+	# scientists who are identified as managers, but who are not registered users of Clean.Data.
+	unreg_manager = models.ForeignKey("UnregScientist",related_name="managed_datasets",null=True,blank=True)
+	# scientists who are identified as managers, and who are registered users of Clean.Data.
 	manager = models.ForeignKey("UserProfile",related_name="managed_datasets",null=True,blank=True)
 	managing_organization = models.ForeignKey("Organization",related_name="managed_datasets",null=True,blank=True)
 
-	#def url_string(self):
-	#	# return name+date
-	
 	def __unicode__(self):
 		return self.name
+	class Meta:
+		unique_together = (('name','url'),)
+
 
 class DataRelation(models.Model):
 	source = models.ForeignKey(Dataset, related_name='relation_to_derivative')
 	derivative = models.ForeignKey(Dataset, related_name='relation_to_source')
-	how_data_was_processed = models.TextField(max_length=20000)
+	how_data_was_processed = models.TextField(max_length=20000,blank=True)
 
 	def __unicode__(self):
 		return self.source.name+" -> "+self.derivative.name
 
 
 # The membership/organisation models
+
+# This is a class to store a reference to a datascientist that's not registered as a user of the site
+# This will be most of the scientists.
+# Note that the natural key is first, last, and profile url. This should keep things more or less unique.
+class UnregScientistManager(models.Manager):
+	def get_by_natural_key(self,firstname,lastname,profile_url):
+		return self.get(firstname=firstname,lastname=lastname,profile_url=profile_url)
+class UnregScientist(models.Model):
+	objects = UnregScientistManager()		# handles natural keys
+	
+	firstname = models.CharField(max_length=30)
+	lastname = models.CharField(max_length=30,blank=True)
+	profile_url = models.URLField(max_length=150,blank=True,default="")
+	class Meta:
+		unique_together = (("firstname","lastname","profile_url"),)
+
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	collaborators = models.ManyToManyField('self')
 	def __unicode__(self):
 		return self.user.name
 	
+class OrganizationManager(models.Manager):
+	def get_by_natural_key(self,name):
+		return self.get(name=name)
 class Organization(models.Model):
-	name = models.CharField(max_length=200)
+	objects = OrganizationManager()		# handles natural keys
+
+	name = models.CharField(max_length=200,unique=True)
+	url = models.URLField(max_length=150,default="")
 	def __unicode__(self):
 		return self.name
 
@@ -79,7 +125,7 @@ class MembershipRelation(models.Model):
 class ContributorRelation(models.Model):
 	contributor = models.ForeignKey(UserProfile, related_name='relation_to_data')
 	dataset = models.ForeignKey(Dataset, related_name='relation_to_contributor')
-	work_done = models.TextField(max_length=20000)
+	work_done = models.TextField(max_length=20000, blank=True)
 	
 	def __unicode__(self):
 		return self.contributor.name+" -> "+self.dataset.name	
