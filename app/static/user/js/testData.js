30baeadc1c87838542efcd2610e3542339b18750
jonroberts <jproberts00@gmail.com>
2013-08-02 09:23:26 -0700

    Added tree view to dataset

---
+++ app/static/user/js/testData.js
@@ -0,0 +1,3 @@
+var derivatives={"name": "zipcodes GeoJSON","children": [{"name": "Energy Zip","size":120},{"name": "VisioNYC","size":120},{"name":"Spread of the West Nile Virus","size":120}]};
+
+var sources={"name": "zipcodes GeoJSON","children": [{"name": "Zipcodes Shapefile","size":120},{"name": "QGIS","size":120}]};
