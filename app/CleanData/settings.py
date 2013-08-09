c4f4572bcf8e62290bc5d29806adc4e4f1dfb1ef
kirankoduru <crazycreature11@gmail.com>
2013-08-09 14:19:57 -0400

    removed template dirs from settings.py

--- app/CleanData/settings.py
+++ app/CleanData/settings.py
@@ -78,13 +78,6 @@ ROOT_URLCONF = 'CleanData.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'CleanData.wsgi.application'
 
-TEMPLATE_DIRS = (
-    "/home/kiran/workspace/cleandata/app/templates",
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
