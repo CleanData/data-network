ceb6454fefdd8d044cc5f3fdcccf463dbf003c29
kirankoduru <crazycreature11@gmail.com>
2013-08-09 14:10:47 -0400

    deleted files in registration app

--- app/CleanData/registration_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from registration.backends.default.views import RegistrationView
-
-
-urlpatterns = patterns('registration_app.views',
-    url(r'^registration/', 'registration'),
-    (r'^',include('registration.backends.default.urls')),
-)
