19fa76e405daea2df1a566ab4ff8512e0f9bf99b
jonroberts <jproberts00@gmail.com>
2013-08-09 13:25:25 -0700

    Added tempalates and views for formats, license and scientists

--- app/CleanData/import_data.sh
+++ app/CleanData/import_data.sh
@@ -5,3 +5,4 @@ python manage.py loaddata organizations.json
 python manage.py loaddata scientists.json
 python manage.py loaddata datacatalogs.json
 python manage.py loaddata datasets.json
+python manage.py loaddata datarelations.json
